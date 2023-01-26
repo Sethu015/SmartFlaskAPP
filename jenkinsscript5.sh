20700878ca2921018039d75deb2db8dcf117e140
Sethu015 <88558500+Sethu015@users.noreply.github.com>
2023-01-26 12:00:36 +0530

    Update jenkinsscript5.sh

--- jenkinsscript5.sh
+++ jenkinsscript5.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-export PATH="~/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-
-pyenv versions
-
-pyenv global 3.10.0
-python3 -m venv myenv --without-pip
-source myenv/bin/activate
 python3 -V
 
 echo '#### Install requirements ####'
@@ -18,8 +9,3 @@ pip install pytest-cov
 echo '#### Run tests ####'
 pytest --cov=main utests --junitxml=./xmlReport/output.xml
 coverage xml -o coverage.xml
-
-echo '### deactivate virtual environment ###'
-deactivate
-
-pyenv global system
